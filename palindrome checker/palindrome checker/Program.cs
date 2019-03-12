7e338fb8fbb736c0c80e2378cb15f96beb7ad642
Vikramjeet singh Singh <30014464@student.toiohomai.ac.nz>
2019-03-13 11:36:22 +1300

    msg

---
+++ palindrome checker/palindrome checker/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Name: Vikramjeet Singh
+Purpose Palindrome Checker*/
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************Palindrome checker***************\n");
+
+            Console.Write("Please enter the first letter: ");
+            string char1 = Console.ReadLine().ToLower();
+            Console.Write("Please enter the second letter: ");
+            string char2 = Console.ReadLine().ToLower();
+            Console.Write("Please enter the third letter: ");
+            string char3 = Console.ReadLine().ToLower();
+
+            if (char2 != "a" && char2 != "e" && char2 != "i" && char2 != "o" && char2 != "u")
+                Console.WriteLine("Your characters dont make a valid word...");
+            else
+
+            if (char1 == char3)
+                Console.WriteLine("Your characters form a proper palindrome...");
+            else
+                Console.WriteLine("Your characters DO NOT forms a proper palindrome...");
+
+
+            Console.WriteLine("Application finished...");
+            Console.ReadLine();
+        }
+    }
+}
